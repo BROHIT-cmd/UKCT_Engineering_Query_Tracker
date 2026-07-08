534a2cb51bca73491b891e32e8ccd8aa0275db73
BROHIT-cmd <Bharat.rohit@xylem.com>
2026-07-08 16:14:14 +0530

    Delete utils directory

--- utils/email_config.py
+++
@@ -1,8 +0,0 @@
-
-ENGINEERS = {
-    "Bharat Rohit": "Bharat.Rohit@Xylem.com",
-    "Manan Kachhiya": "Manan.Kachhiya@xylem.com",
-    "Harh Shrimankar": "Harsh.Shrimankar@xylem.com",
-    "Abhishek Bhavsar": "Abhishek.Bhavsar@xylem.com",
-    "Rachit Marsonia": "Rachit.Marsonia@xylem.com"
-}
