7f7b4e953f2ecca678af83c3eeaa64f0eabfd16e
BROHIT-cmd <Bharat.rohit@xylem.com>
2026-07-08 16:14:02 +0530

    Create engineers.py

---
+++ engineers.py
@@ -0,0 +1,7 @@
+ENGINEERS = {
+    "Bharat Rohit": "Bharat.rohit@xylem.com",
+    "Harsh Shrimankar": "Harsh.Shrimankar@xylem.com",
+    "Rachit Marsonia": "Rachit.Marsonia@xylem.com",
+    "Abhishek Bhavsar": "Abhishek.Bhavsar@xylem.com",
+    "Manan Kachhiya": "Manan.Kachhiya@xylem.com"
+}
