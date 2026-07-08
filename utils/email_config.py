c7f9770fe28fd5294373c7696ff6db4d217296de
BROHIT-cmd <Bharat.rohit@xylem.com>
2026-07-08 15:35:01 +0530

    Create email_config.py

---
+++ utils/email_config.py
@@ -0,0 +1,8 @@
+
+ENGINEERS = {
+    "Bharat Rohit": "Bharat.Rohit@Xylem.com",
+    "Manan Kachhiya": "Manan.Kachhiya@xylem.com",
+    "Harh Shrimankar": "Harsh.Shrimankar@xylem.com",
+    "Abhishek Bhavsar": "Abhishek.Bhavsar@xylem.com",
+    "Rachit Marsonia": "Rachit.Marsonia@xylem.com"
+}
